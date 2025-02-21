b59d12ad9d1e81b12bd6635436e94a7c12d8f8bd
Vitor Guilherme R. <162996534+Kyra4code@users.noreply.github.com>
2025-02-20 22:29:07 -0300

    Inicialização da barra de navegação

--- pw3-somativa-2025-1/src/App.jsx
+++ pw3-somativa-2025-1/src/App.jsx
@@ -1,18 +1,11 @@
-import BookCards from "./components/BookCards.jsx"
 import './App.css';
-import capa_livro from "./assets/image.png";
-import Button from "./components/Button.jsx"
+import NavBar from './components/layout/NavBar';
 
 function App() {
   return (
     <>
       <div>
-        <h1>PW3-WEBAPP-LIVRARIA</h1>
-        <BookCards
-        title="Bom dia"
-        autor="Kyra4code"
-        imagem={capa_livro}
-        />
+        <NavBar/>
       </div>
     </>
   )
---
+++ pw3-somativa-2025-1/src/components/layout/NavBar.jsx
@@ -0,0 +1,16 @@
+
+import style from "./NavBar.module.css"
+
+const NavBar = ()=>{
+    return(
+        <nav>
+            <ul>
+                <li>HOME</li>
+                <li>CADASTRAR LIVROS</li>
+                <li>LISTAR LIVROS</li>
+            </ul>
+        </nav>
+    )
+}
+
+export default NavBar
