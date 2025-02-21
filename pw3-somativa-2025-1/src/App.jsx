d1b0b91fe35192aa23d7472edeea0f17c40168f5
Vitor Guilherme R. <162996534+Kyra4code@users.noreply.github.com>
2025-02-20 21:52:05 -0300

    Estilização do BookCard e criação e estilização do button

--- pw3-somativa-2025-1/src/App.jsx
+++ pw3-somativa-2025-1/src/App.jsx
@@ -1,33 +1,19 @@
-import { useState, useEffect } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
+import BookCards from "./components/BookCards.jsx"
 import './App.css';
+import capa_livro from "./assets/image.png";
+import Button from "./components/Button.jsx"
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <h1>PW3-WEBAPP-LIVRARIA</h1>
+        <BookCards
+        title="Bom dia"
+        autor="Kyra4code"
+        imagem={capa_livro}
+        />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
---
+++ pw3-somativa-2025-1/src/components/BookCards.jsx
@@ -0,0 +1,15 @@
+import style from './BookCards.module.css'
+import Button from './Button.jsx'
+
+const BookCards = ({title, autor, imagem})=>{
+    return(
+        <div className={style.bookCards}>
+            <h3 className={style.title}>{title}</h3>
+            <p className={style.autor}>{autor}</p>
+            <img src={imagem} alt='Uma imagem' className={style.imagem}/>
+            <Button label="DETALHE"/>
+        </div>
+    )
+}
+
+export default BookCards
