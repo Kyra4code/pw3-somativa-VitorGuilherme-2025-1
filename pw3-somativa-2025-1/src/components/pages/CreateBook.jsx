df4304106fa289bbc7f47cb13e9d19de2e2d3550
Kyra4code <lucarioecharmito@gmail.com>
2025-02-27 21:28:58 -0300

    Foi craido a navegação por paginas utilizando o react-router-dom em aula online pelo teams

---
+++ pw3-somativa-2025-1/src/components/pages/CreateBook.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import style from "./createBook.module.css";
+
+const CreateBook = () => {
+    return(
+        <section>
+            <h1>Create book</h1>
+        </section>
+    )
+}
+
+export default CreateBook;
